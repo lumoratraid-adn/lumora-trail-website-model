5d4f54e042f4ea0f717394a3a06dab0187fc2648
Dhanish P <dhanish0777@gmail.com>
2026-02-09 00:36:29 +0400

    change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -98,17 +98,10 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/logo.svg",
+        url: "/icon3.png",
         type: "image/svg+xml",
-      },
+        sizes: "any",
+      }
     ],
     apple: "/apple-icon.png",
   },
